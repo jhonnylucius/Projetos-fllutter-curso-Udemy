11b96bf4dace2db0e386354a21660f2e4aaadd6f
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2024-12-24 12:54:48 -0300

    adiciona exemplo de uso de loop while em Dart

---
+++ loopWhile/loopWhileExemplo.dart
@@ -0,0 +1,11 @@
+void main() {
+  int count = 0;
+
+  // O loop while continua executando enquanto a condição for verdadeira
+  while (count < 5) {
+    print('O valor de count é: $count');
+    count++; // Incrementa o valor de count em 1 a cada iteração
+  }
+}
+// O loop while continua executando enquanto a condição for verdadeira
+// O valor de count é: 0
