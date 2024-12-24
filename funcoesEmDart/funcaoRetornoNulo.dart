44ea3e0c78423bda3810b6528e6a2b4d3746ee28
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2024-12-24 16:35:35 -0300

    adiciona função que retorna nulo em Dart no arquivo funcaoRetornoNulo.dart

---
+++ funcoesEmDart/funcaoRetornoNulo.dart
@@ -0,0 +1,11 @@
+void main() {
+  // função
+  final retornoNulo = getMelhorMusica(); // chamando a função
+
+  print(retornoNulo);
+}
+
+String? getMelhorMusica() {
+  // função que retorna nulo
+  return 'null';
+}
