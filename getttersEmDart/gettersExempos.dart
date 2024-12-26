882c18306844fdb3418c588f430d1a52d5cd9df3
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2024-12-26 15:55:42 -0300

    adiciona exemplos de getters e setters em Dart com a classe Carro

---
+++ getttersEmDart/gettersExempos.dart
@@ -0,0 +1,49 @@
+void main() {
+  final gol = Carro(cor: 'azul', tamanho: 3.80); // Instanciando a classe Carro
+
+  print(gol.cor); // Carro Branco
+  gol.ligar(); // chamando o metodo ligar
+  print(gol.estaLigado()); // true
+  print(gol.retornaTamanho()); // 8
+
+  print(gol.altura);
+  gol.modificaAltura(15);
+  print(gol.altura);
+}
+
+class Carro {
+  // variaveis = propriedades
+  // funções = metodos
+  final String cor;
+  final double tamanho;
+
+  int _altura = 4; // variavel privada
+  final int _largura = 2; // variavel privada
+
+  int get altura => _altura; // getter para acessar a variavel privada
+
+  Carro(
+      {required this.cor,
+      required this.tamanho}); // chamando o metodo construtor
+
+  int retornaTamanho() {
+    return _altura *
+        _largura; // chamando o metodo privado que é acessivel dentro do arquivo, mesmo
+    // sendo privado, mas são realmente privados quando tentados acessar fora do arquivo.
+  } // metodo privado
+
+  void ligar() {
+    // metodo ligar
+    print('Carro ligado');
+  }
+
+  bool estaLigado() {
+    // metodo
+    return true;
+  }
+
+  void modificaAltura(int novaAltura) {
+    // setter para modificar a variavel privada
+    _altura = novaAltura;
+  }
+}
