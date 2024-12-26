2986316a78b225900c8e27cd4b1c2e11dc118215
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2024-12-26 18:08:03 -0300

    atualiza exemplos de setters em Dart, adiciona métodos get e set para altura na classe Carro

--- settersEmDart/settersExemplos.dart
+++ settersEmDart/settersExemplos.dart
@@ -5,6 +5,8 @@ void main() {
   gol.ligar(); // chamando o metodo ligar
   print(gol.estaLigado()); // true
   print(gol.retornaTamanho()); // 8
+  print(gol.altura); // 5
+  gol.setAltura = 6; // chamando o metodo setAltura
 }
 
 class Carro {
@@ -13,26 +15,44 @@ class Carro {
   final String cor;
   final double tamanho;
 
-  final int _altura = 4; // variavel privada
-  int _largura = 2; // variavel privada
+  int _altura = 5; // variavel privada
+  final int _largura = 2; // variavel privada
+
+  int get altura =>
+      _altura; // metodo get altura informando que é privado de leitura
+
+  set setAltura(int altura) {
+    // metodo set altura informando que é privado de escrita
+    if (altura >= 5) {
+      _altura = altura;
+    } else {
+      print('Altura inválida');
+    }
+  }
+
+  void modifigaAltura(int altura) {
+    // metodo modificaAltura
+    // regras seria colocadas aqui
+    _altura = altura; // atribuindo o valor da variavel privada
+  }
 
   Carro(
       {required this.cor,
       required this.tamanho}); // chamando o metodo construtor
 
   int retornaTamanho() {
-    return _altura *
+    return _altura * // metodo privado
         _largura; // chamando o metodo privado que é acessivel dentro do arquivo, mesmo
     // sendo privado, mas são realmente privados quando tentados acessar fora do arquivo.
-  } // metodo privado
+  }
 
   void ligar() {
-    // metodo
+    // metodo ligar com o metodo print
     print('Carro ligado');
   }
 
   bool estaLigado() {
-    // metodo
+    // metodo estaLigado com o metodo print
     return true;
   }
 }
