9ed3c60f3f1ad2c27efbd55ae77777fe28f2b970
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-01 11:10:00 -0300

    Adicionar configuração do Firebase e dependências no projeto

--- pj1/pj1/lib/main.dart
+++ pj1/pj1/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pj1/firebase_options.dart';
 import 'package:pj1/screens/login_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
