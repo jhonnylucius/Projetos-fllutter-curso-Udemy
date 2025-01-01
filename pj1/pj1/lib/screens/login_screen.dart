4c1330b40a1ace1ce8a112f9a337cbc410518efe
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-01 00:26:44 -0300

    Adicionar campo de senha e botões de ação na tela de login

--- pj1/pj1/lib/screens/login_screen.dart
+++ pj1/pj1/lib/screens/login_screen.dart
@@ -4,6 +4,7 @@ class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
 
   final TextEditingController _emailController = TextEditingController();
+  final TextEditingController __senhaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +28,38 @@ class LoginScreen extends StatelessWidget {
                     SizedBox(height: 16.0),
                     TextField(
                       controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: 'E-mail',
-                      ),
-                    )
+                      decoration: InputDecoration(labelText: 'E-mail'),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextField(
+                      obscureText: true,
+                      controller: __senhaController,
+                      decoration: InputDecoration(labelText: 'Senha'),
+                    ),
+                    SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Entrar'),
+                    ),
+                    SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Entrar com Google'),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Criar uma conta!'),
+                    ),
+                    SizedBox(height: 16.0),
+                    Text(
+                      'Esqueceu a senha?',
+                      style: TextStyle(color: Colors.blue),
+                      softWrap: true,
+                    ),
                   ],
                 ),
-              )
+              ),
             ],
           ),
         ),
