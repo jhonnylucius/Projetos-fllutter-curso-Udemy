14495ba659a5d8701aa285f3b8514108fe033fa4
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-04 18:56:49 -0300

    adiciona campo 'nome' na classe Hour e atualiza métodos relacionados

--- pj1/pj1/lib/models/hour.dart
+++ pj1/pj1/lib/models/hour.dart
@@ -3,19 +3,22 @@ class Hour {
   String data;
   int minutos;
   String? descricao;
+  String nome;
 
   Hour({
     required this.id,
     required this.data,
     required this.minutos,
     required this.descricao,
+    required this.nome,
   });
 
   Hour.fromMap(Map<String, dynamic> map)
       : id = map['id'],
         data = map['data'],
         minutos = map['minutos'],
-        descricao = map['descricao'];
+        descricao = map['descricao'],
+        nome = map['nome'];
 
   Map<String, dynamic> toMap() {
     return {
@@ -23,6 +26,7 @@ class Hour {
       'data': data,
       'minutos': minutos,
       'descricao': descricao,
+      'nome': nome,
     };
   }
 }
