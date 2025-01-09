9dbee5395b920d4323b9d0b60ee13d2e289b121e
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-08 22:05:16 -0300

    altera a classe Expenses para incluir campos descricaoDaReceita e tipoReceita, removendo campos obsoletos

--- pj1/pj1/lib/models/expenses.dart
+++ pj1/pj1/lib/models/expenses.dart
@@ -1,40 +1,32 @@
 class Expenses {
   String id;
   String data;
-  int minutos;
-  String? descricao;
-  String nome;
   double preco;
-  String? type; // Add this line
+  String? descricaoDaReceita;
+  String tipoReceita;
 
   Expenses({
     required this.id,
     required this.data,
-    required this.minutos,
-    required this.descricao,
-    required this.nome,
     required this.preco,
-    this.type, // Add this line
+    required this.descricaoDaReceita,
+    required this.tipoReceita,
   });
 
   Expenses.fromMap(Map<String, dynamic> map)
       : id = map['id'],
         data = map['data'],
-        minutos = map['minutos'],
-        descricao = map['descricao'],
-        nome = map['nome'],
         preco = map['preco'].toDouble(),
-        type = map['type']; // Add this line
+        descricaoDaReceita = map['descricao'],
+        tipoReceita = map['tipoReceita'];
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'data': data,
-      'minutos': minutos,
-      'descricao': descricao,
-      'nome': nome,
       'preco': preco,
-      'type': type, // Add this line
+      'descricaoDaReceita': descricaoDaReceita,
+      'tipoReceita': tipoReceita,
     };
   }
 }
