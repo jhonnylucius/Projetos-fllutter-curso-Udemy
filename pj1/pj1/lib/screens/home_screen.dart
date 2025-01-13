6c2a57a8627e5da7008102dc9ffc9ec85385bc95
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-13 09:43:07 -0300

    Atualizar tela inicial e metadados do aplicativo para refletir novo nome e descrição

--- pj1/pj1/lib/screens/home_screen.dart
+++ pj1/pj1/lib/screens/home_screen.dart
@@ -38,67 +38,75 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Menu(user: widget.user),
-      appBar: AppBar(
-        title: Text('GestorFinanceiro'),
-        elevation: 2,
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        foregroundColor: Colors.white,
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/background_home_screen.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Stack(
-          children: [
-            Positioned(
-              top: 20,
-              left: 0,
-              right: 0,
-              child: Center(
-                child: Text(
-                  'Bem-vindo, $displayName',
-                  style: TextStyle(
-                      fontSize: 24,
-                      color: const Color.fromARGB(255, 123, 21, 141),
-                      fontWeight: FontWeight.w700),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+            drawer: Menu(user: widget.user),
+            appBar: AppBar(
+              title: Text('GestorFinanceiro'),
+              elevation: 2,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              foregroundColor: Colors.white,
+            ),
+            body: Container(
+              constraints: BoxConstraints(maxWidth: 480),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/background_home_screen.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 20,
+                      left: 0,
+                      right: 0,
+                      child: Center(
+                        child: Text(
+                          'Bem-vindo, $displayName',
+                          style: TextStyle(
+                              fontSize: 24,
+                              color: const Color.fromARGB(255, 123, 21, 141),
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          FloatingActionButton.extended(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/costs');
+                            },
+                            icon: const Icon(Icons.add, color: Colors.white),
+                            label: const Text('Despesas',
+                                style: TextStyle(color: Colors.white)),
+                            backgroundColor:
+                                Theme.of(context).colorScheme.secondary,
+                          ),
+                          SizedBox(height: 16),
+                          FloatingActionButton.extended(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/expenses');
+                            },
+                            icon: const Icon(Icons.add, color: Colors.white),
+                            label: const Text('Receitas',
+                                style: TextStyle(color: Colors.white)),
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primary,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  FloatingActionButton.extended(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/costs');
-                    },
-                    icon: const Icon(Icons.add, color: Colors.white),
-                    label: const Text('Despesas',
-                        style: TextStyle(color: Colors.white)),
-                    backgroundColor: Theme.of(context).colorScheme.secondary,
-                  ),
-                  SizedBox(height: 16),
-                  FloatingActionButton.extended(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/expenses');
-                    },
-                    icon: const Icon(Icons.add, color: Colors.white),
-                    label: const Text('Receitas',
-                        style: TextStyle(color: Colors.white)),
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+            ));
+      },
     );
   }
 }
