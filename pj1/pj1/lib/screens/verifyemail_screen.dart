473825f8f9e1630e5b127a15c85efacf2dce2bee
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-16 11:54:26 -0300

    Substituir verificação de email por StreamBuilder e remover lógica de temporizador

--- pj1/pj1/lib/screens/verifyemail_screen.dart
+++ pj1/pj1/lib/screens/verifyemail_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -14,29 +12,6 @@ class VerifyEmailScreen extends StatefulWidget {
 
 class VerifyEmailScreenState extends State<VerifyEmailScreen> {
   bool isVerifying = false;
-  Timer? _timer;
-
-  @override
-  void initState() {
-    super.initState();
-    _startEmailVerificationCheck();
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
-  }
-
-  void _startEmailVerificationCheck() {
-    _timer = Timer.periodic(Duration(seconds: 5), (timer) async {
-      await widget.user.reload();
-      if (widget.user.emailVerified) {
-        timer.cancel();
-        Navigator.pushReplacementNamed(context, '/home');
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,55 +20,68 @@ class VerifyEmailScreenState extends State<VerifyEmailScreen> {
         title: Text('Verifique seu Email'),
         automaticallyImplyLeading: false, // Remove a seta de voltar
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Um email de verificação foi enviado para ${widget.user.email}.',
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: isVerifying
-                  ? null
-                  : () async {
-                      setState(() {
-                        isVerifying = true;
-                      });
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            User? user = snapshot.data;
+            if (user != null && user.emailVerified) {
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                Navigator.pushReplacementNamed(context, '/home');
+              });
+            }
+          }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Um email de verificação foi enviado para ${widget.user.email}.',
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: isVerifying
+                      ? null
+                      : () async {
+                          setState(() {
+                            isVerifying = true;
+                          });
 
-                      await widget.user.reload();
-                      if (widget.user.emailVerified) {
-                        Navigator.pushReplacementNamed(context, '/home');
-                      } else {
-                        setState(() {
-                          isVerifying = false;
-                        });
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('Email ainda não verificado.'),
-                            backgroundColor: Colors.red,
-                          ),
-                        );
-                      }
-                    },
-              child: Text('Já verifiquei meu email'),
+                          await widget.user.reload();
+                          if (widget.user.emailVerified) {
+                            Navigator.pushReplacementNamed(context, '/home');
+                          } else {
+                            setState(() {
+                              isVerifying = false;
+                            });
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('Email ainda não verificado.'),
+                                backgroundColor: Colors.red,
+                              ),
+                            );
+                          }
+                        },
+                  child: Text('Já verifiquei meu email'),
+                ),
+                SizedBox(height: 16.0),
+                TextButton(
+                  onPressed: () async {
+                    await widget.user.sendEmailVerification();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Email de verificação reenviado.'),
+                        backgroundColor: Colors.green,
+                      ),
+                    );
+                  },
+                  child: Text('Reenviar email de verificação'),
+                ),
+              ],
             ),
-            SizedBox(height: 16.0),
-            TextButton(
-              onPressed: () async {
-                await widget.user.sendEmailVerification();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('Email de verificação reenviado.'),
-                    backgroundColor: Colors.green,
-                  ),
-                );
-              },
-              child: Text('Reenviar email de verificação'),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
